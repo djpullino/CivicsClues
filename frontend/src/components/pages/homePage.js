10bd2f46226f1dbfdea5970004e933da58d0dbc0
Allan Brockenbrough <brockenbrough@gmail.com>
2024-08-22 09:09:07 -0400

    Added CSS styling to project.  Applied to profile page (home).

--- frontend/src/components/pages/homePage.js
+++ frontend/src/components/pages/homePage.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import getUserInfo from '../../utilities/decodeJwt'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import getUserInfo from '../../utilities/decodeJwt';
+
 const HomePage = () => {
-    const [user, setUser] = useState({})
-    const navigate = useNavigate()
+    const [user, setUser] = useState({});
+    const navigate = useNavigate();
+
     const handleClick = (e) => {
         e.preventDefault();
-        localStorage.removeItem('accessToken')
-        return navigate('/')
-    }
+        localStorage.removeItem('accessToken');
+        return navigate('/');
+    };
 
     useEffect(() => {
-        setUser(getUserInfo())
-    }, [])
-
+        setUser(getUserInfo());
+    }, []);
 
     if (!user) return (
-        <div><h4>Log in to view this page.</h4></div>)
-    const { id, email, username, password } = user
+        <div><h4>Log in to view this page.</h4></div>
+    );
+
+    const { id, email, username } = user;
+
     return (
         <>
-            <div>
-                <h3>
-                    Welcome
-                    <span className='username'> @{username}</span>
-                </h3>
-                <h3>
-                    Your userId in mongo db is
-                    <span className='userId'> {id}</span>
-                </h3>
-                <h3>
-                    Your registered email is
-                    <span className='email'> {email}</span>
-                </h3>
-                <h3>
-                    Your password is
-                    <span className='password'> {password} ( hashed )</span>
-                </h3>
+            <div className="card-container">
+                <div className="card">
+                    <h3>Welcome</h3>
+                    <p className="username">{username}</p>
+                </div>
+                <div className="card">
+                    <h3>Your userId in MongoDB is</h3>
+                    <p className="userId">{id}</p>
+                </div>
+                <div className="card">
+                    <h3>Your email is</h3>
+                    <p className="email">{email}</p>
+                </div>
             </div>
             <button onClick={(e) => handleClick(e)}>
                 Log Out
             </button>
         </>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
