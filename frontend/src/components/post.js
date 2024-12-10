db25e726a6ac54500a71ced8aff43dbed31fdd99
djpullino <djpullino6503@gmail.com>
2024-12-10 18:51:20 -0500

    changed post format

--- frontend/src/components/post.js
+++ frontend/src/components/post.js
@@ -6,19 +6,22 @@ const Post = ({ post }) => {
 
   return (
     <div className="mb-4 p-4 text-[#301952] max-w-lg">
-      <div className="flex justify-between items-center mb-2">
-       <div className="text-lg font-semibold text-[#301952]">
-          @{post.username} - Currently {post.party}
+      <div className="flex justify-center items-center mb-2">
+        <div className="text-lg font-semibold text-[#301952]">
+          @{post.username}
         </div>
-        
       </div>
-      <span className="text-sm text-gray-600">{formattedDate}</span>
-
-      <div className="post-content mb-4">
-        <p className="text-lg break-words">{post.content}</p>
+      <span className="pb-6 text-md text-gray-600 mb-8">
+        <span className="font-bold text-[#301952]">{post.party}</span> at {formattedDate}
+      </span> 
+      <div className="post-content mb-8">
+        <p className="text-xl break-words">{post.content}</p>
       </div>
     </div>
   );
+  
+  
+  
 };
 
 export default Post;
